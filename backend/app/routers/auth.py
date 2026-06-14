dd2621051cf14e1f937ad188f24c9f7fb2e55192
khizra <khizrahanif95@gmail.com>
2026-06-14 02:55:26 -0700

    RP-26 Add registration and login API endpoints

---
+++ backend/app/routers/auth.py
@@ -0,0 +1,53 @@
+from fastapi import APIRouter, Depends, HTTPException
+from fastapi.security import OAuth2PasswordRequestForm
+from sqlalchemy.orm import Session
+
+from app.database.session import get_db
+from app.schemas.user import UserCreate, UserResponse
+from app.services.user_service import get_user_by_email, create_user
+from app.schemas.user import UserLogin
+from app.services.user_service import authenticate_user
+from app.core.security import create_access_token
+
+#JWT (JSON Web Token) is a signed token used to identify a user without storing session data on the server.
+
+router = APIRouter()
+
+
+@router.post("/register", response_model=UserResponse)
+def register_user(user_data: UserCreate, db: Session = Depends(get_db)):
+    existing_user = get_user_by_email(db, user_data.email)
+
+    if existing_user:
+        raise HTTPException(
+            status_code=400,
+            detail="Email already registered"
+        )
+
+    return create_user(db, user_data)
+
+@router.post("/login")
+def login(
+    form_data: OAuth2PasswordRequestForm = Depends(),
+    db: Session = Depends(get_db)
+):
+    user = authenticate_user(
+        db,
+        form_data.username,
+        form_data.password
+    )
+
+    if user is None:
+        raise HTTPException(
+            status_code=401,
+            detail="Invalid email or password"
+        )
+
+    access_token = create_access_token(
+        data={"sub": user.email}
+    )
+
+    return {
+        "access_token": access_token,
+        "token_type": "bearer"
+    }
